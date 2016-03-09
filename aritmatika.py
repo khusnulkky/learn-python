16a7a60d55f2ad3945219c99144c142e828ab2d6
khusnulkky <Knesya@rocketmail.com>
2016-03-09 14:31:16 +0800

    first story

---
+++ aritmatika.py
@@ -0,0 +1,27 @@
+print "operator aritmatika"
+print ""
+a = 3
+b = 15.5
+c = 20
+d = 11
+
+penjumlahan = a + c
+pengurangan = c - d
+pembagian = c / d
+perkalian = a * c * d
+modulus = c % a
+exponent = a ** d
+rata_rata = a + b + c + d / 4
+print "penjumlahan", penjumlahan
+print ""
+print "pengurangan", pengurangan
+print ""
+print "pembagian", pembagian
+print ""
+print "perkalian", perkalian
+print ""
+print "modulus", modulus
+print ""
+print "exponent", exponent
+print ""
+print "rata_rata", rata_rata
---
+++ variabel.py
@@ -0,0 +1,7 @@
+name		=	"khusnul"		# A string
+old			=	19				# An integer
+birth_date	=	"10 11 1996"	# A string
+
+print name
+print old
+print birth_date
